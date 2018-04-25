34a98cabf72a8f78acc1775a12c83b10502db276
Njerschow <nj18@nyu.edu>
2018-04-25 08:26:05 -0400

    finished acquiring models (still have to run on all 99 models)

--- Cross.py
+++ Cross.py
@@ -2,6 +2,8 @@ import sys
 import numpy as np
 import csv
 
+WIGGLE_ROOM=0.003
+
 reuseCtr = 0;
 num = []
 name = []
@@ -19,7 +21,7 @@ with open('./lenses.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
     	if (reuseCtr!=0):
-    		num.append(row[0])
+    		num.append(int(row[0])-1) # minus one to make this a valid index for ~~indexing~~
     		name.append(row[1])
     		grade.append(row[2])
     		sourcez.append(row[3])
@@ -37,16 +39,34 @@ with open('./lenses.csv') as csvfile:
     		dconv = float(ddata[0]) + float(ddata[1])/60. + float(ddata[2])/3600
     		if (negative==1):
     			dconv = -1.*dconv
-    		print(dconv)
     		dec.append(dconv)
 
     		mags.append(row[7])
     		magl.append(row[8])
     		Nim.append(row[9])
-    		size.append(row[10])
+    		size.append(float(row[10])*WIGGLE_ROOM) #convert from arcseconds to degrees (with extra wiggle room)
     	reuseCtr+=1
 
 
+queries = open("Queries.txt","w") 
+
+queries.seek(0)
+queries.truncate() # empty file before writing
+
+for i in num:
+	ratop = ra[i]+size[i]  #Defining rectangle of interest 
+	rabot = ra[i]-size[i]
+	dectop = dec[i]+size[i]
+	decbot = dec[i]-size[i]
+	queries.write("java -jar casjobs.jar run 'SELECT ALL p.objid,p.ra,p.dec,pz.z INTO mydb.object_"+str(i)+" FROM PhotoObj AS p JOIN Photoz AS pz ON pz.objid = p.objid where p.ra between "+str(rabot)+" and "+str(ratop)+" and p.dec between "+str(decbot)+" and "+str(dectop)+"' \n\n")
+	# and now to download table:
+	queries.write("java -jar casjobs.jar extract -b object_"+str(i)+" -F -type CSV -d ./Models/\n\n")
+	# and now to drop table remotely:
+	queries.write("java -jar casjobs.jar execute -t 'mydb/1' 'drop table object_"+str(i)+"'\n\n")
+
+queries.close()
+
+print("donezo")
 # ra = 0
 # if (sys.argv[4]=="ra"):
 # 	ra = 1
--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Lenses.csv is a table containing recently discovered astronomical lensed objects
 
 Cross.py is a python script that takes no arguments and reads from Lenses.csv.
 The script will generate queries that I will be able to run on the SDSS Casjobs command line tool to produce tables containing primitive Einstein Cross models
+
+The Models directory contains models that were generated using the queries in Queries.txt
